5a94389c29a20fa29238eb9e490d3f2496173c41
cedrick keumaleu <keumaleucedrick@gmail.com>
2023-05-23 10:13:49 +0200

    mon prémier commite

--- modal.js
+++ modal.js
@@ -28,23 +28,33 @@ myForm.addEventListener('submit', function(e){
   e.preventDefault()
 
   //création des variables et récupération id Name et errorName
-  const errorFirstName = document.getElementById('errorFirstName');
   const firstName = document.getElementById('first');
   const lastName = document.getElementById('last');
-  const errorLastName = document.getElementById('errorLastName')
+  const myEmail = document.getElementById('email');
+  const myBirthdate = document.getElementById('birthdate');
+  const partycipationQuantity = document.getElementById('quantity');
+  const radioButton = document.getElementById('location');
+  const myCheckbox = document.getElementById('checkbox')
+
+  const errorFirstName = document.getElementById('errorFirstName');
+  const errorLastName = document.getElementById('errorLastName');
+  const errorEmail = document.getElementById('errorEmail');
+  const errorBirthdate = document.getElementById('errorBirthdate');
+  const errorQuantity = document.getElementById('errorQuantity');
+  const errorRadio = document.getElementById('errorRadio')
+  const errorCheckbox = document.getElementById('errorCheckbox')
   
   //création de la variable de vérification avec une expréssion régulière
   //pour le prenom et le nom
-  const myRegExp = /^[a-zA-Z-\s]+$/;
+  const myRegExp = /^[a-zA-Z-\s]{2,15}$/;
 
   // condition de validation du champ prenom
-  console.log(firstName.value)
   if (firstName.value.trim()===""){
     errorFirstName.innerHTML = "ce champ est obligatoire.";
     errorFirstName.style.color = 'red';
     e.preventDefault();
   } else if (myRegExp.test(firstName.value) ===false) {
-    errorFirstName.innerHTML = " le nom doit comporter des lettres, des tirets uniquement.";
+    errorFirstName.innerHTML = " le prenom doit comporter au moins 02 lettres, des tirets uniquement si possible.";
     errorFirstName.style.color = 'red';
     e.preventDefault();
   }else{
@@ -53,13 +63,12 @@ myForm.addEventListener('submit', function(e){
 
 
   // condition de validation du champ nom
-  console.log(lastName.value)
   if (lastName.value.trim()===""){
     errorLastName.innerHTML = "ce champ est obligatoire.";
     errorLastName.style.color = 'red';
     e.preventDefault();
   } else if (myRegExp.test(lastName.value) ===false) {
-    errorLastName.innerHTML = " le nom doit comporter des lettres, des tirets uniquement.";
+    errorLastName.innerHTML = " le prenom doit comporter au moins 02 lettres, des tirets uniquement si possible..";
     errorLastName.style.color = 'red';
     e.preventDefault();
   }else{
@@ -67,4 +76,66 @@ myForm.addEventListener('submit', function(e){
   }
 
   // vérification de email
-})
+  const emailRegExp = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+
+   // condition de validation du champ nom
+   console.log(myBirthdate.value)
+   if (myEmail.value.trim()===""){
+     errorEmail.innerHTML = "ce champ est obligatoire.";
+     errorEmail.style.color = 'red';
+     e.preventDefault();
+   } else if (emailRegExp.test(myEmail.value) ===false) {
+     errorEmail.innerHTML = " veuillez saisir un adresse mail valide.";
+     errorEmail.style.color = 'red';
+     e.preventDefault();
+   }else{
+     errorEmail.innerHTML = ""
+   }
+
+   // vérification de date
+  //  const myDate = new Date();
+        //  month = myDate.getMonth()+ 01;
+        //  date = myDate.getDate();
+        //  day = myDate.getDay();
+
+   if (myBirthdate.value.trim()===""){
+    errorBirthdate.innerHTML = "ce champ est obligatoire.";
+    errorBirthdate.style.color = 'red';
+    e.preventDefault();
+   }else {
+    errorBirthdate.innerHTML = ""
+   }
+
+   // vérification radio bouton
+   if (getRadioButton(form.elements["location"]) === undefined){
+    valide = false;
+    errorRadio.innerHTML = "veuillez sélectionner une localisation";
+    errorRadio.style.color = 'red'
+   }else{
+    errorRadio.innerHTML =""
+   }
+
+   // vérification checkbox
+   if (getCheckbox(form.elements["accord"]).length === 0){
+    valide = false;
+    errorCheckbox.innerHTML = "veuillez cocher un ou plusiers champ"
+    errorCheckbox.style.color = 'red'
+   }else{
+    errorCheckbox.innerHTML = ""
+   }
+
+   //si la validation est en erreur, les données ne sont pas envoyées
+   const valide = true;
+   if (!valide){
+    e.preventDefault(); //empeche les données être transfére au serveur
+   }
+});
+
+function getRadioButton(radioBouton){
+  let choix;
+  for (let radio of radioBouton){
+    if (radio.checked){
+      choix = radio.value;
+    }
+  }
+}
